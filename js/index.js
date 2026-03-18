eb888594012f34c3f65ba9b01d6b3c1487f42e9f
Anna <annapaschenko828@gmail.com>
2026-03-18 14:44:42 +0200

    Add projects section with filter functionality

--- js/index.js
+++ js/index.js
@@ -6,3 +6,32 @@ const menu = document.querySelector('.nav__menu');
 burger.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
+
+
+// Js for the list in the projects section
+
+const filterLinks = document.querySelectorAll('[data-filter]');
+const cards = document.querySelectorAll('[data-category]');
+const line = document.querySelector('.projects__filter-line');
+
+filterLinks[0].classList.add('active');
+
+filterLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const category = link.dataset.filter;
+
+        cards.forEach(card => {
+            if (category === 'all' || card.dataset.category === category) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+
+        filterLinks.forEach(f => f.classList.remove('active'));
+        link.classList.add('active');
+        link.prepend(line);
+    });
+});
